c41837120813476ce725153ed4e4bd249e9b8bfc
zimkjh <zimkjh@naver.com>
2020-12-05 18:44:43 +0900

    하단 배너 광고 추가 (높이 100px)
    https://github.com/zimkjh/IdolWorldCup/issues/5

--- worldcup/lib/pages/landing.dart
+++ worldcup/lib/pages/landing.dart
@@ -1,9 +1,35 @@
+import 'package:firebase_admob/firebase_admob.dart';
 import 'package:flutter/material.dart';
+import '../adManager.dart';
 import 'question.dart';
 
 class LandingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    FirebaseAdMob.instance.initialize(appId: AdManager.appId);
+
+    MobileAdTargetingInfo targetingInfo = MobileAdTargetingInfo(
+      keywords: <String>['idol', 'k-pop', 'kpop', 'music'],
+      contentUrl: 'https://flutter.io',
+      childDirected: false,
+      testDevices: <String>[], // Android emulators are considered test devices
+    );
+
+    BannerAd myBanner = BannerAd(
+      adUnitId: BannerAd.testAdUnitId,
+      size: AdSize.smartBanner,
+      targetingInfo: targetingInfo,
+      listener: (MobileAdEvent event) {
+        print("BannerAd event is $event");
+      },
+    );
+
+    myBanner
+      ..load()
+      ..show(
+        anchorType: AnchorType.bottom,
+      );
+
     return new Material(
       color: Colors.purple,
       child: new InkWell(
